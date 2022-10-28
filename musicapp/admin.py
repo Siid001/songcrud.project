a5149b709309db268da85172ab8197936e725410
Siid001 <awoteduoluwaseun@gmail.com>
2022-10-28 11:23:38 +0100

    Added modified admin.py, models.py and settings.py

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Artiste, Song, Lyrics
+
+@admin.register(Artiste)
+class FireFlyAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age')
+
+@admin.register(Song)
+class FireFlyAdmin(admin.ModelAdmin):
+    list_display = ( 'title', 'date_released', 'likes', 'artisteId')
+
+@admin.register(Lyrics)
+class FireFlyAdmin(admin.ModelAdmin):
+    list_display = ('content', 'songid')
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,29 @@
-from django.db import models
 
+from django.db import models
+from django.conf import settings
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length= 100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete= models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    date_released = models.DateField()
+    likes  = models.IntegerField()
+    artisteId = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Lyrics(models.Model):
+    Song = models.ForeignKey(Song, on_delete= models.CASCADE)
+
+    content = models.CharField(max_length=500)
+    songid = models.IntegerField()
